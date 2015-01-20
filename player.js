45042f95ee6477da3c9c45cf4b7526b1bcc603eb
Ubuntu-QA <eyal@showbox.com>
2015-01-20 18:21:55 +0200

    better

--- player.js
+++ player.js
@@ -1,178 +1,282 @@
-var Players = function() {
-    var playersReadyCounter = 0,
-        loadInterval = {},
-        durations = [];
+// Functions
+function init(numPlayers) {
+    for (var i = 0; i < numPlayers; ++i) {
+        var f = $('<input>', {'class': 'f' + i, 'type': 'text', 'data-id': '#player' + i}),
+            label = $('<label>player ' + i + ':</label>');
 
-    this.getMaxDuration = function() {
-        return Math.min.apply(null, durations);
+        f.appendTo(label);
+        label.appendTo('.fields');
+        if (localStorage['f' + i]) {
+            $('.f' + i).val(localStorage['f' + i]);
+        }
     }
-    
+}
+
+function attachButtonEventListeners(players, GE) {
+    $('.load').on('click', function(e) {
+        // $('.next').show();
+        console.log('load');
+        var urls = [],
+            hasSrc = 0;
+
+        for (var i = 0; i < players.getNumPlayers(); ++i) {
+            var el = $('.f' + i);
+            urls.push(el.val());
+            if (el.val() !== '') {
+                hasSrc += 1;
+            }
+        }
+
+        // verifyUrls(urls, players);
+        players.populatePlayers(urls, hasSrc);
+        resetControls();
+    });
+
+    $('.next').on('click', function(e) {
+        console.log('next');
+        var urls = [],
+            baseUrl = 'http://showbox-vs-sanity.s3.amazonaws.com/output/',
+            clip = $('.clip option:selected').next('option').val();
+
+        if (typeof(clip) !== 'undefined') {
+            $('.clip').val(clip);
+            for (var i = 0; i < players.getNumPlayers(); ++i) {
+                var v = $('.v' + i).val();
+                urls.push(baseUrl + v + '/' + clip + '.mp4');
+            }
+            verifyUrls(urls);
+        } else {
+            alert('no more clips!');
+        }
+    });
+
+    $('.add').on('click', function(e) {
+        console.log('add player');
+        var f = $('<input>', {'class': 'f' + players.getNumPlayers(), 'type': 'text', 'data-id': '#player' + players.getNumPlayers()}),
+            label = $('<label>player ' + players.getNumPlayers() + ':</label>');
+
+        f.appendTo(label);
+        label.appendTo('.fields');
+        var className = 'f' + players.getNumPlayers();
+        if (localStorage[className]) {
+            $('.f' + className).val(localStorage[className]);
+        }
+        players.addPlayer();
+        attachVideoEventListeners(GE);
+        // $('.load').click();
+    });
+
+    $('.remove').on('click', function(e) {
+        console.log('removing player');
+        var n = players.getNumPlayers() - 1;
+        $('.f' + n).parent().remove();
+        players.removePlayer();
+    });
+
+    $('.save').on('click', function(e) {
+        console.log('saving');
+        localStorage.setItem('numPlayers', players.getNumPlayers());
+        $('label input').each(function() {
+            localStorage.setItem($(this).attr('class'), $(this).val());
+        });
+    });
+
+    $('#play').on('click', function() {
+        if ($(this).val() === 'play') {
+            console.log('play click');
+            $(this).val('pause');
+            GE.trigger('playVideo');
+        } else if ($(this).val() === 'pause') {
+            console.log('pause click');
+            $(this).val('play');
+            GE.trigger('pauseVideo');
+        } else {
+            console.log('replay');
+            $(this).val('pause');
+            GE.trigger('playVideo');
+        }
+    });
+
+    $('#seek').on('input', function() {
+        var timestamp = $(this).val();
+        console.log('time seeked: ' + timestamp);
+        GE.trigger('updateTime', [timestamp]);
+        $('#duration').val(prettyTime(parseInt(timestamp, 10)));
+    });
+
+    $('video').on('timeupdate', function(e) {
+        var timestamp = e.target.currentTime;
+        // console.log(timestamp);
+        $('#seek').val(timestamp);
+        $('#duration').val(prettyTime(parseInt(timestamp, 10)));
+    });
+
+    $('video').on('ended', function(e) {
+        console.log('ENDED!!');
+        $('#play').val('replay');
+    });
+}
+
+function attachVideoEventListeners(GE) {
+    GE.off();
+    // these events should trigger for all players simultaneously
+    $('video').each(function(index, el) {
+        GE.on('playVideo', function(e) {
+            el.play();
+        });
+        GE.on('pauseVideo', function(e) {
+            el.pause();
+        });
+        GE.on('updateTime', function(e, time) {
+            el.currentTime = time;
+        });
+    });
+    $('video').on('loadedmetadata', function(e) {
+        $('input[type="range"]').prop('max', e.target.duration);
+    });
+};
+
+function verifyUrls(urls, players) {
+    var err = [],
+        deferreds = [];
+
+    for (var i = 0; i < urls.length; ++i) {
+        deferreds.push(
+            $.ajax(urls[i])
+            .fail(function() {
+                err.push(this.url);
+            })
+        );
+    }
+
+    $.when.apply($, deferreds)
+    .then(function() {
+        console.log('all urls are valid');
+        players.populatePlayers(urls);
+        resetControls();
+    }, function() {
+        console.log('invalid urls: ' + err);
+        err = err.map(function(x) {return x.split('output/').pop()});
+        alert('the following videos do not exist: \n' + err.join('\n'));
+    });
+}
+
+function prettyTime(secs) {
+    var minutes = Math.floor(secs / 60),
+        seconds = secs - minutes *60;
+
+    return (('0' + minutes).slice(-2) + ':' + ('0' + seconds).slice(-2));
+}
+
+function resetControls() {
+    $('#play').val('play');
+    $('#duration').val('00:00');
+    $('#seek').val(0);
+}
+
+
+// Video Players class
+var Players = function(num) {
+    var playersReadyCounter = 0,
+        numPlayers = num,
+        parent = '.videoWrapper',
+        currentId = 0,
+        loadInterval = {};
+
+    this.attachPlayers = function() {
+        var n = numPlayers - $(parent).children().length;
+
+        for (var i = 0; i < n; ++i) {
+            var v = $('<video>', {
+                'class': 'col-md-6',
+                'id': 'player' + currentId,
+                'src': '',
+                'preload': 'auto'
+            });
+            v.appendTo(parent);
+            currentId += 1;
+        }
+    }
+
     this.enableControls = function() {
-        $("#play").prop("disabled", false);
-        $("#seek").prop("disabled", false);
+        $('#play').prop('disabled', false);
+        $('#seek').prop('disabled', false);
     }
 
     this.disableControls = function() {
-        $("#play").prop("disabled", true);
-        $("#seek").prop("disabled", true);
+        $('#play').prop('disabled', true);
+        $('#seek').prop('disabled', true);
     }
-    
-    this.waitForLoad = function(el, index) {
-        if ($(el).prop("readyState")) {
-            var duration = $(el).prop("duration"),
-                buffered = $(el).prop("buffered").end(0);
+
+    this.waitForLoad = function(el, index, nPlayers) {
+        if ($(el).prop('readyState')) {
+            var duration = $(el).prop('duration'),
+                buffered = $(el).prop('buffered').end(0);
 
             if (buffered === duration) {
-                console.log("video " + index + " is fully loaded.");
+                console.log('video ' + index + ' is fully loaded.');
                 playersReadyCounter += 1;
-                if (playersReadyCounter === 2) {
+                if (playersReadyCounter === nPlayers) {
                     this.enableControls();
                 }
                 clearInterval(loadInterval[index]);
-                // console.log("interval cleared");
+                // console.log('interval cleared');
                 delete loadInterval[index];
             }
         }
     }
-    
-    this.populatePlayers = function(urls) {
+
+    this.populatePlayers = function(urls, hasSrc) {
         this.disableControls();
         playersReadyCounter = 0;
 
-        $("#player1").prop("src", urls[0]);
-        $("#player2").prop("src", urls[1]);
+        for (var i = 0; i < numPlayers; ++i) {
+            $('#player' + i).prop('src', urls[i])
+        }
 
-        console.log("alakazam!");
-        $(".videoWrapper").show();
-        $(".controls").show();
+        console.log('alakazam!');
+        $('.videoWrapper').show();
+        $('.controls').show();
 
         var that = this;
-
-        $("video").each(function(index, el) {
-            durations.push(el.duration);
+        $('video').each(function(index) {
             this.load();
-            $(this).on("canplaythrough", function(){
-                this.play();
-                this.pause();
-                clearInterval(loadInterval[index])
-                loadInterval[index] = setInterval(that.waitForLoad.bind(that, this, index), 500);
-                $(this).off("canplaythrough");
-            });
+            if ($(this).attr('src') !== '') {
+                $(this).on('canplaythrough', function(){
+                    this.play();
+                    this.pause();
+                    clearInterval(loadInterval[index]);
+                    loadInterval[index] = setInterval(that.waitForLoad.bind(that, this, index, hasSrc), 500);
+                    $(this).off('canplaythrough');
+                });
+            }
         });
+    }
+
+    this.addPlayer = function() {
+        numPlayers += 1;
+        this.attachPlayers();
+    }
+
+    this.removePlayer = function() {
+        numPlayers -= 1;
+        currentId -= 1;
+        $('#player' + currentId).remove();
+    }
+
+    this.getNumPlayers = function() {
+        return numPlayers;
     }
 }
 
 
 $(document).ready(function() {
-    var players = new Players(),
-        clips = $(".clip option").each(function() {arr.push($(this).val())});
-        GE = $("body");
+    var numPlayers = parseInt(localStorage['numPlayers'], 10) || 1,
+        players = new Players(numPlayers),
+        GE = $('body');
 
-    $(".videoWrapper").hide();
-    $(".controls").hide();
-
-    $(".load").on("click", function(e) {
-        console.log("load");
-        var urls = [];
-
-        $("input[type=text]").each(function() {
-            urls.push($(this).val());
-        })
-
-        console.log(urls)
-        // verifyUrls(urls);
-        players.populatePlayers(urls);
-        resetControls();
-    });
-
-    (function attachEventListeners() {
-        $("video").each(function(index, el) {
-            // these events should trigger for all players simultaneously
-            GE.on("playVideo", function(e) {
-                el.play();
-            });
-            GE.on("pauseVideo", function(e) {
-                el.pause();
-            });
-            GE.on("updateTime", function(e, time) {
-                el.currentTime = time;
-            });
-        });
-        // seekbar max duration is set to the shortest video's duration
-        $("video").on("loadedmetadata", function(e) {
-            console.log("video duration: ");
-            console.log(e.target.duration);
-            if ($("input[type='range']").prop("max") === 0.0) $("input[type='range']").prop("max", e.target.duration);
-            if (e.target.duration < $("input[type='range']").prop("max")) $("input[type='range']").prop("max", e.target.duration);
-        });
-    })();
-
-    function prettyTime(secs) {
-        var minutes = Math.floor(secs / 60),
-            seconds = secs - minutes *60;
-
-        return (("0" + minutes).slice(-2) + ":" + ("0" + seconds).slice(-2));
-    }
-
-    function resetControls() {
-        $("#play").val("play");
-        $("#duration").val("00:00");
-        $("#seek").val(0);
-    }
-
-    // events:
-    $("#play").on("click", function() {
-        if ($(this).val() === "play") {
-            console.log("play click");
-            $(this).val("pause");
-            GE.trigger("playVideo");
-        } else if ($(this).val() === "pause") {
-            console.log("pause click");
-            $(this).val("play");
-            GE.trigger("pauseVideo");
-        } else {
-            console.log("replay");
-            $(this).val("pause");
-            GE.trigger("playVideo");
-        }
-    });
-
-    $("#seek").on("input", function() {
-        var timestamp = $(this).val();
-        console.log("time seeked: " + timestamp);
-        GE.trigger("updateTime", [timestamp]);
-        $("#duration").val(prettyTime(parseInt(timestamp, 10)));
-    });
-
-    $("video").on("timeupdate", function(e) {
-        var timestamp = e.target.currentTime;
-        // console.log(timestamp);
-        $("#seek").val(timestamp);
-        $("#duration").val(prettyTime(parseInt(timestamp, 10)));
-    });
-
-    $("video").on("ended", function(e) {
-        console.log("ENDED!!");
-        $("#play").val("replay");
-    });
-
-    // maybe someday I'll try to make the buffering thing work properly
-
-    // $("video").on("waiting", function(e) {
-    //     var index = e.target.id;
-    //     if (loadedArray[index] !== 0) {
-    //         loadedArray[index] = 0;
-    //         console.log("video " + index + " is waiting");
-    //         GE.trigger("pauseVideo");
-    //         GE.trigger("updateTime", [e.target.currentTime]);
-    //     }
-    // });
-
-    // $("video").on("canplay", function(e) {
-    //     var index = e.target.id;
-    //     loadedArray[index] = 1;
-    //     console.log(index + " is ready");
-    //     if (loadedArray.indexOf(0) === -1 && play) GE.trigger("playVideo");
-    // });
-
+    players.attachPlayers();
+    attachButtonEventListeners(players, GE);
+    attachVideoEventListeners(GE);
+    init(players.getNumPlayers());
 });
